843eb1e9cadfb8b8f5dca817061a4b055e2cb31a
quocduyvu6262 <quocduyvu6262@gmail.com>
2024-11-11 16:32:03 -0600

    Enable sim

--- resco_benchmark/main.py
+++ resco_benchmark/main.py
@@ -24,7 +24,7 @@ def main():
                              ])
     ap.add_argument("--pwd", type=str, default=os.path.dirname(__file__))
     ap.add_argument("--log_dir", type=str, default=os.path.join(os.path.dirname(os.getcwd()), 'results' + os.sep))
-    ap.add_argument("--gui", type=bool, default=False)
+    ap.add_argument("--gui", type=bool, default=True)
     ap.add_argument("--libsumo", type=bool, default=False)
     ap.add_argument("--tr", type=int, default=0)  # Can't multi-thread with libsumo, provide a trial number
     ap.add_argument("--save_freq", type=int, default=100)
