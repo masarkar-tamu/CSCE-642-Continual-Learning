fix total vehicle calculation and fractional demand generation

resco_benchmark/utils/add_flow.py
@@ -3,14 +3,14 @@ import numpy as np
 import copy
 import os
 import random
+import logging
 
 from resco_benchmark.config.config import config as cfg
-
 
 # TODO rewrite
 def generate_additional_flow():
     map_path = os.path.join(os.path.dirname(cfg.network))
-    fractional = cfg.flow % 1
+    fractional = cfg.flow - int(cfg.flow)
     multiple = int(cfg.flow)
 
     try:
@@ -22,22 +22,14 @@ def generate_additional_flow():
             multiple = multiple + 1
 
         i = 0
-        removals, child_set = [], []
+        child_set = []
         total_vehicles = 0
         for child in root:
             if child.tag == "trip" or child.tag == "vehicle":
-                child.set("departLane", "free")
-                total_vehicles += 1
-                # if fractional != 0 and multiple == 0:
-                #     if np.random.random() > fractional:
-                #         removals.append(child)
-                #     continue  # This is just getting really bad, srry
                 if "_v" in child.attrib["id"]:
                     continue
-                # if fractional != 0:
-                #     if np.random.random() > fractional:
-                #         i += 1
-                #         continue
+                child.set("departLane", "free")
+                total_vehicles += 1
                 for j in range(1, multiple):
                     new_child = copy.deepcopy(child)
 
@@ -46,16 +38,15 @@ def generate_additional_flow():
                     total_vehicles += 1
                     i += 1
                 child_set.append((child, i))
+                
             i += 1
-
         if fractional != 0:
             sampled_list = random.sample(child_set, int(len(child_set) * fractional))
             if multiple != 0:
-                # multiple = multiple - 1
                 for child, position in sampled_list:
                     new_child = copy.deepcopy(child)
 
-                    new_child.attrib["id"] = new_child.attrib["id"] + "_v1" 
+                    new_child.attrib["id"] = new_child.attrib["id"] + "_v" + str(multiple) 
                     root.insert(position + 1, new_child)
                     total_vehicles += 1
             else: